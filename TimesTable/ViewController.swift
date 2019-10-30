76ef47eec52ea6585ce7f123233d626e3e98385a
Tatiane <guedestdgp@outlook.com>
2019-10-30 16:42:03 -0400

    Calculate is working and showing results.

--- TimesTable/ViewController.swift
+++ TimesTable/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var numbers: [Int]?
+    @IBOutlet weak var txNUmberField: UITextField!
+    @IBOutlet weak var tvShowResults: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard numbers != nil else {return UITableViewCell()}
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "")
+        cell.textLabel?.text = "\(numbers![indexPath.row])"
+        return cell
+    }
+    
+    @IBAction func btActionCalculate(_ sender: UIButton) {
+        numbers = [Int]()
+        let number = Int(txNUmberField.text!) ?? 0
+        for item in 1...10 {
+            numbers?.append(number * item)
+        }
+        tvShowResults.reloadData()
+    }
 }
 
